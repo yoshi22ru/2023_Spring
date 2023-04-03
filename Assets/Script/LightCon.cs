d92a9709c4bbcb55696f9d3a99d0af2077f694bd
yoshi22ru <o.yoshikaze@gamil.com>
2023-04-04 00:17:50 +0900

    Stamina_Bar,LookAt_Target

---
+++ Assets/Script/LightCon.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCon : MonoBehaviour
+{
+    public GameObject targetObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(targetObject.transform);
+    }
+}
--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -10,6 +10,9 @@ public class Move : MonoBehaviour
 {
     private Animator animator;
 
+    public float stamina;
+    public Slider stamina_slider;
+
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -20,12 +23,41 @@ public class Move : MonoBehaviour
         if(Input.GetKey(KeyCode.UpArrow)||Input.GetKey(KeyCode.W))
         {
             animator.SetBool("Walk", true);
-            transform.position += transform.forward * 0.05f;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.position += transform.forward * 0.08f;
+                stamina = stamina - (Time.deltaTime * 10);
+            }
+            else
+            {
+                transform.position +=transform.forward* 0.05f;
+            }
         }
         else
         {
             animator.SetBool("Walk", false);
         }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("Run", true);
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+        }
+
+        if(!animator.GetCurrentAnimatorStateInfo(0).IsName("Run"))
+        {
+            stamina = stamina + (Time.deltaTime * 4);
+        }
+
+        const float MIN = 0;
+        const float MAX = 100;
+
+        stamina = Mathf.Clamp(stamina, MIN, MAX);
+        stamina_slider.value = stamina;
         
         if(Input.GetKey(KeyCode.DownArrow)||Input.GetKey(KeyCode.S))
         {
